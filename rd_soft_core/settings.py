2253a999f08a7acb1151032e7a2053cd911222ce
Rajat <85779110+rajatDeveloper@users.noreply.github.com>
2023-10-07 13:08:06 +0530

    Update settings.py

--- rd_soft_core/settings.py
+++ rd_soft_core/settings.py
@@ -44,6 +44,14 @@ INSTALLED_APPS = [
     "corsheaders",
 ]
 
+
+
+
+
+
+
+
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
@@ -135,7 +143,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-CORS_ORIGIN_ALLOW_ALL = True
+
+
+
+
+
 # CORS_ORIGIN_WHITELIST = ("http://localhost:3000/",)
 # CORS_ALLOWED_ORIGINS = ["http://localhost:3000/"]
 
